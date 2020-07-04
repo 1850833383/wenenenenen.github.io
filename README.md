d41ca6d030dc2fc5a51c9de1ae583e3f78ae5e7d
Wenenenenen <787372368@qq.com>
2020-07-04 18:13:50 +0800

    添加北京疫情图

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Personal Blog
 现在是一个简单的响应式网站模板
 等待着我去修饰一番
 1 gei wo li giao giao
+
+mapbox_token:pk.eyJ1Ijoiemhpd2VuaiIsImEiOiJjand2eGlzb2MwYWg3NDlyMXNmbGJyZGh2In0.VCR4KV-QWW2vBugH2G6cDw
---
+++ assets/js/map.js
@@ -0,0 +1,193 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoiemhpd2VuaiIsImEiOiJjand2eGlzb2MwYWg3NDlyMXNmbGJyZGh2In0.VCR4KV-QWW2vBugH2G6cDw';
+var map,style;
+var curr_yq = {};
+
+// var style = {
+//     "version": 8,
+//     "sources": {
+//         "bj_geoJson": {
+//             "type": "geojson",
+//             "data": "https://geo.datav.aliyun.com/areas_v2/bound/110000_full.json"
+//         }
+//     },
+//     "layers": []
+// };
+//疫情数据
+getJson('https://lab.isaaclin.cn/nCoV/api/area?latest=1&province=北京市').then(function(yqjson){
+    if(yqjson.success){
+        curr_yq = yqjson;
+        let properties = yqjson.results[0].cities;
+        //北京geojson
+        getJson('https://geo.datav.aliyun.com/areas_v2/bound/110000_full.json').then(function(bjgeojson){
+            let features = bjgeojson.features;
+                    for (let p = 0; p < properties.length; p++) {
+                        const city = properties[p];
+                        for (let f = 0; f < features.length; f++) {
+                            const feature = features[f];
+                            if(city.locationId == feature.properties.adcode){
+                                feature.properties.currentConfirmedCount = city.currentConfirmedCount;
+                                feature.id = feature.properties.adcode;
+                                    if(city.currentConfirmedCount == 0){//1<= deliverNumber && deliverNumber<= 8
+                                        feature.properties.yqcolor = '#ffffff';
+                                    }else if(0 < city.currentConfirmedCount && city.currentConfirmedCount < 10){
+                                        feature.properties.yqcolor = '#ffe5db';
+                                    }else if(10 <= city.currentConfirmedCount && city.currentConfirmedCount < 50){
+                                        feature.properties.yqcolor = '#ffc4b3';
+                                    }else if(50 <= city.currentConfirmedCount && city.currentConfirmedCount < 100){
+                                        feature.properties.yqcolor = '#ff9985';
+                                    }else if(100 <= city.currentConfirmedCount && city.currentConfirmedCount < 1000){
+                                        feature.properties.yqcolor = '#f57567';
+                                    }else if(1000 <= city.currentConfirmedCount){
+                                        feature.properties.yqcolor = '#e64546';
+                                    }
+                            }else if(feature.properties.adcode == 110117){
+                                feature.properties.yqcolor = '#ffffff';
+                            }
+                            
+                        }
+                    }
+                    style = {
+                         "version": 8,
+                         "sources": {
+                             "bj_geoJson": {
+                                 "type": "geojson",
+                                 "data": bjgeojson
+                             }
+                         },
+                         "layers": [],
+                         "glyphs": "mapbox://fonts/mapbox/{fontstack}/{range}.pbf"
+                    }
+
+                    map = new mapboxgl.Map({
+                        container: 'map',
+                        style: style, // stylesheet location
+                        center: [116.38475021500989, 39.92216392261716], // starting position [lng, lat]
+                        zoom: 5 // starting zoom
+                    });
+
+                    map.flyTo({
+                        zoom: 8,
+                        speed: 0.2,
+                        pitch: 40
+                    });
+                    var hoveredStateId =  null;
+                    map.once("styledata",function(){
+                        map.addLayer({
+                            "id": "beijing_fill",
+                            "type": "fill",
+                            "source": "bj_geoJson",
+                            "layout": {
+                        
+                            },
+                            'paint': {
+                                'fill-color': ['get','yqcolor'],
+                                "fill-opacity": ["case",
+                                    ["boolean", ["feature-state", "hover"], false],
+                                    1,
+                                    0.4
+                                ]
+                            }
+                        });
+                        map.addLayer({
+                            "id": "beijing_line",
+                            "type": "line",
+                            "source": "bj_geoJson",
+                            "layout": {
+                    
+                            },
+                            'paint': {
+                                'line-color': 'black',
+                                'line-width': 0.5
+                            }
+                        });
+                        map.addLayer({
+                            "id": "beijing_symbol",
+                            "type": "symbol",
+                            "source": "bj_geoJson",
+                            "layout": {
+                                "text-field": ['format',
+                                ['downcase', ['get', 'name']], { 'font-scale': .6 }],
+                            }
+                        });
+                    });
+                    
+                    var popup = new mapboxgl.Popup({
+                        closeButton: false,
+                        closeOnClick: false,
+                        offset:[0,-10]
+                    });
+                    
+                    map.on("mousemove", "beijing_fill", function(e) {
+                        if (e.features.length > 0) {
+                            let geometry = e.features[0].geometry;
+
+                            if (hoveredStateId) {
+                                map.setFeatureState({source: 'bj_geoJson', id: hoveredStateId}, { hover: false});
+                            }
+                            hoveredStateId = e.features[0].id;
+                            map.setFeatureState({source: 'bj_geoJson', id: hoveredStateId}, { hover: true});
+
+                            // if(map.getSource('city_yellowSource')){
+                            //     if(map.getLayer('city_yellowLayer')){
+                            //         map.removeLayer('city_yellowLayer')
+                            //     }
+                            //     map.removeSource('city_yellowSource')
+                            // }
+                            // map.addSource('city_yellowSource',{
+                            //     'type': 'geojson',
+                            //     'data': {
+                            //         'type': 'Feature',
+                            //         'geometry': geometry
+                            //     }
+                            // });
+                            // map.addLayer({
+                            //     "id": "city_yellowLayer",
+                            //     "type": "fill",
+                            //     "source": "city_yellowSource",
+                            //     "layout": {
+                            
+                            //     },
+                            //     'paint': {
+                            //         'fill-color': 'yellow',
+                            //         "fill-opacity": 0.8
+                            //     }
+                            // });
+                            var name = e.features[0].properties.name;
+                            var curr = e.features[0].properties.currentConfirmedCount;
+                            if(!curr){
+                                curr = 0;
+                            }
+                            let des = '<div>'+name+'</div><div>现存确诊</div><div style="font-size:20px;color:red;">'+curr+'人</div>';
+                            popup.setLngLat(e.lngLat)
+                            .setHTML(des)
+                            .addTo(map);
+                        }
+                    });
+                    
+                    map.on('mouseleave', "beijing_fill", function() {
+
+                        // if(map.getSource('city_yellowSource')){
+                        //     if(map.getLayer('city_yellowLayer')){
+                        //         map.removeLayer('city_yellowLayer')
+                        //     }
+                        //     map.removeSource('city_yellowSource')
+                        // }
+                        if (hoveredStateId) {
+                            map.setFeatureState({source: 'bj_geoJson', id: hoveredStateId}, { hover: false});
+                        }
+                        hoveredStateId =  null;
+
+                        popup.remove();
+                    });
+        });
+    }
+});
+
+//同步获取数据
+function getJson(url) {
+    return new Promise(function(resolve, reject) {
+        $.get(url, (response) => {
+            resolve(response);
+        });
+    });
+}
